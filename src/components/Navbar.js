aacf27d92b372f6bfbe05b1880f51be676ba12f2
Yeison <enmanuelquiterios@gmail.com>
2021-11-19 23:40:49 -0500

    Completed

---
+++ src/components/Navbar.js
@@ -0,0 +1,25 @@
+import {useState} from 'react'
+import {Link} from 'react-router-dom'
+
+function NavBar({handleLogin}) {
+
+  const [loggedIn, setLoggedIn] = useState(false)
+
+  return(
+    <>
+      <nav className="navbar">
+
+      <span>Spendsy <i class="fas fa-coins"></i></span>
+
+      <div className="navlink-buttons">
+            {loggedIn ? <Link to='/login'><button  className="navlink" onClick={() => setLoggedIn(!loggedIn)}>Logout</button></Link> : 
+            <Link to= "/signIn"><button className="navlink"onClick={() => setLoggedIn(!loggedIn)}>Login</button></Link>}
+        <Link to='/'>
+          <button className="navlink">Home</button>
+        </Link>
+        </div>
+      </nav>
+    </>
+  )
+}
+export default NavBar
